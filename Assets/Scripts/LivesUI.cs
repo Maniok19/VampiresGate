662fe1c9323f484469f40f51dfd009edb3836af9
Maniok19 <mano19029@hotmail.fr>
2020-11-27 15:10:59 +0100

    initial
    ajout des bases de prototypage du jeu

---
+++ Assets/Scripts/LivesUI.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class LivesUI : MonoBehaviour {
+
+    public Text livesTexte;
+
+	void Update () {
+        livesTexte.text = PlayerStats.lives + " LIVES";
+	}
+}
